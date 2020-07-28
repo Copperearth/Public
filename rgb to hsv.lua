7901affe56025aba0ffac88a83984fe9e2acaeca
Copper <feodor.engels@gmail.com>
2020-07-28 21:56:55 +0200

    rgb to hsv

---
+++ rgb to hsv.lua
@@ -0,0 +1,23 @@
+local function rgb_to_hsv(r, g, b)
+	r, g, b = r / 255, g / 255, b / 255
+	local max, min = math.max(r, g, b), math.min(r, g, b)
+	local h, s, v
+	v = max
+
+	local d = max - min
+	if max == 0 then s = 0 else s = d / max end
+
+	if max == min then
+		h = 0 -- achromatic
+	else
+		if max == r then
+		h = (g - b) / d
+		if g < b then h = h + 6 end
+		elseif max == g then h = (b - r) / d + 2
+		elseif max == b then h = (r - g) / d + 4
+		end
+		h = h / 6
+	end
+
+	return h, s, v
+end
