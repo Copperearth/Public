0f3489681ee0775bfb840a16a3a5e01539ec4de3
Introvert1337 <66886179+Introvert1337@users.noreply.github.com>
2020-07-28 16:38:58 -0400

    Create FindFirstDescendantWhichIsA.lua

---
+++ Misc/FindFirstDescendantWhichIsA.lua
@@ -0,0 +1,25 @@
+local mt = getrawmetatable(game)
+setreadonly(mt, false)
+local oldNamecall = mt.__namecall
+
+mt.__namecall = newcclosure(function(self,...)
+    local args = {...}
+    local method = getnamecallmethod()
+    if method == "FindFirstDescendantWhichIsA" then 
+        if #args == 0 then 
+            return error("No argument was provided")
+        end
+        if #args > 1 and type(args[1]) ~= "string" then 
+            return error("First argument must be a string")
+        end
+        for i,v in next, self:GetDescendants() do 
+            if v:IsA(args[1]) then 
+                return v 
+            end
+        end
+        return nil
+    end
+    return oldNamecall(self,...)
+end)
+
+setreadonly(mt, true)
