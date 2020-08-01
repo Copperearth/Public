78f4218f91220f016f7420eb26db739aa39f1e99
Introvert1337 <66886179+Introvert1337@users.noreply.github.com>
2020-08-01 00:12:01 -0400

    Update CommonDenominator.lua

--- Math/CommonDenominator.lua
+++ Math/CommonDenominator.lua
@@ -1,15 +1,6 @@
 setreadonly(math, false)
 
 math.cd = function(a, b)
-    if type(a) ~= "number" and type(b) ~= "number" then
-        return error("Arguments 1 and 2 must be integers")
-    end
-    if type(a) ~= "number" then
-        return error("Argument 1 must be an integer")
-    end
-    if type(b) ~= "number" then
-        return error("Argument 2 must be an integer")
-    end
     local common = {}
     for i = 1, math.min(a, b) do
         if a/i == math.floor(a/i) and b/i == math.floor(b/i) then
