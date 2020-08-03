c2d21efec072cffae31a9c38a12a1f35d2e3592a
Introvert1337 <66886179+Introvert1337@users.noreply.github.com>
2020-08-03 02:37:07 -0400

    added custom error handling

--- Math/Factorial.lua
+++ Math/Factorial.lua
@@ -1,6 +1,15 @@
 setreadonly(math, false)
 
 math.factorial = function(int)
+    if not int then 
+        return error("Argument 1 must be provided")
+    end
+    if type(int) ~= "number" then 
+        return error("Argument 1 must be an integer")
+    end
+    if int < 0 then 
+        return error("Argument 1 must be a whole number")
+    end 
     local result = 1
     if int > 0 then
         for i = 1, int do
