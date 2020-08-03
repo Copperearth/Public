6c37acc82ed7570331ab63d7ba1498592eda1516
Introvert1337 <66886179+Introvert1337@users.noreply.github.com>
2020-08-03 02:46:46 -0400

    added custom error handling

--- Math/QuadraticFormula.lua
+++ Math/QuadraticFormula.lua
@@ -1,6 +1,24 @@
 setreadonly(math, false)
 
 math.quad = function(a, b, c)
+    if not a then 
+        return error("Argument 1 must be provided")
+    end 
+    if not b then 
+        return error("Argument 2 must be provided")
+    end 
+    if not c then 
+        return error("Argument 3 must be provided")
+    end
+    if type(a) ~= "number" then 
+        return error("Argument 1 must be an integer")
+    end 
+    if type(b) ~= "number" then 
+        return error("Argument 2 must be an integer")
+    end 
+    if type(c) ~= "number" then 
+        return error("Argument 3 must be an integer")
+    end 
     return 
     (-b + math.sqrt((b * b) - (4 * a * c))) / (2 * a), ",", 
     (-b - math.sqrt((b * b) - (4 * a * c))) / (2 * a)
