9f7f34b370b1dd2ce10de4cc27fab1d89ae80732
Introvert1337 <66886179+Introvert1337@users.noreply.github.com>
2020-08-03 02:47:52 -0400

    Update factors.lua

--- Math/factors.lua
+++ Math/factors.lua
@@ -1,6 +1,12 @@
 setreadonly(math, false)
 
 math.factors = function(int)
+    if not int then 
+        return error("Argument 1 must be provided")
+    end 
+    if type(int) ~= "number" then 
+        return error("Argument 1 must be an integer")
+    end
     local factors = {}
     for firstindex = 1, int do
         for secondindex = 0, int do
