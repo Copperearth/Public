72bb898d74b3edb6f4c163585db059db017031e3
Introvert1337 <66886179+Introvert1337@users.noreply.github.com>
2020-08-03 02:49:30 -0400

    added custom error handling, other stuff, too

--- String/Shuffle.lua
+++ String/Shuffle.lua
@@ -1,13 +1,19 @@
 setreadonly(string, false)
 
 string.shuffle = function(str)
+    if not str then 
+        return error("Argument 1 must be provided")
+    end 
+    if type(str) ~= "string" then 
+        return error("Argument 1 must be a string")
+    end
     local newStr = {}
     repeat
     	for i = 1, string.len(str) do
     		newStr[i] = string.sub(str, i, i)
     	end
     	local index = 0
-    	local temp = nil
+    	local temp
     	for i = #newStr, 1, -1 do
     		index = math.random(1, i)
     		temp = newStr[index]
