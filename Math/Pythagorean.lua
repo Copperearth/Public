31d2fd5e2e0acf37b130d9a8651701c4dadb2bb0
Introvert1337 <66886179+Introvert1337@users.noreply.github.com>
2020-08-03 02:41:32 -0400

    added custom error handling

--- Math/Pythagorean.lua
+++ Math/Pythagorean.lua
@@ -1,6 +1,21 @@
 setreadonly(math, false)
 
 math.pyth = function(a, b)
+    if not a and not b then 
+        return error("Arguments 1 and 2 must be provided")
+    end 
+    if a and not b then 
+        return error("Argument 2 must be provided")
+    end 
+    if type(a) ~= "number" and type(b) ~= "number" then 
+        return error("Arguments 1 and 2 must be integers")
+    end 
+    if type(a) ~= "number" and type(b) == "number" then 
+        return error("Argument 1 must be an integer")
+    end 
+    if type(a) == "number" and type(b) ~= "number" then 
+        return error("Argument 2 must be an integer")
+    end
     return math.sqrt((a * a) + (b * b))
 end
 
